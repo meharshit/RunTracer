7e0f4fcb904192b0a56fd3db8be568fa9f329f73
Harshit Satyaseel <hsatyaseel@gmail.com>
2018-09-22 16:43:49 +0530

    Initial Commit

---
+++ RunTracer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RunTracer
+//
+//  Created by Harshit Satyaseel on 22/09/18.
+//  Copyright © 2018 Harshit Satyaseel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
