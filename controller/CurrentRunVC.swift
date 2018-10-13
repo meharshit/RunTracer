76ac2481912959f860fa43e33ed8c8895a06978f
Harshit Satyaseel <hsatyaseel@gmail.com>
2018-10-13 17:02:19 +0530

    map view initial work done

---
+++ controller/CurrentRunVC.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentRunVC.swift
+//  RunTracer
+//
+//  Created by Harshit Satyaseel on 13/10/18.
+//  Copyright © 2018 Harshit Satyaseel. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+        
+        
+    }
+
+}
--- controller/StartRunVC.swift
+++ controller/StartRunVC.swift
@@ -7,13 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
-class StartRunVC: UIViewController {
 
+class StartRunVC: locationVC {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkLocationAuthStatus()
+        mapView.delegate = self
+        
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
+    }
+    
+    
 
+
+    @IBAction func centreLocationButton(_ sender: Any) {
+    }
+    
 
 }
 
+// we need to extend the class for show the user its location on the map so..
+
+extension StartRunVC : CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse{
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+        
+    }
+    
+}
